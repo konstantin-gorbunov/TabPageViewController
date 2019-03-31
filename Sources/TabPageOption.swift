4b99fa0217327e4bb5825c299bc97b90b9f2851f
Kostya Gorbunov <kgorbunov@viber.com>
2019-03-31 11:06:34 +0200

    - Jeff changes;

--- Sources/TabPageOption.swift
+++ Sources/TabPageOption.swift
@@ -19,6 +19,7 @@ public struct TabPageOption {
 
     public init() {}
 
+    public var defaultFont = UIFont.systemFont(ofSize: UIFont.systemFontSize)
     public var fontSize = UIFont.systemFontSize
     public var currentColor = UIColor(red: 105/255, green: 182/255, blue: 245/255, alpha: 1.0)
     public var defaultColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0)
